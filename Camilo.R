de9926ac8f6d0b3dd3f502df6adfea5ed67fcf80
johanportilla-max <johan.portilla@correounivalle.edu.co>
2025-11-05 17:40:34 -0500

    A

---
+++ Camilo.R
@@ -0,0 +1,51 @@
+library(tidyverse)
+library(readr)
+
+# Cargar la base
+Base <- read_csv("f36a5086-3311-4b1a-9f0c-bda5cd4718df_Series - Metadata.csv")
+
+# Ver estructura
+glimpse(Base)
+
+# Seleccionar solo las variables relevantes y renombrarlas a español
+Base_2022 <- Base %>%
+  select(
+    Pais = `Country Name`,
+    Codigo = `Country Code`,
+    PIB_per = `GDP per capita (current US$) [NY.GDP.PCAP.CD]`,
+    Poblacion = `Population, total [SP.POP.TOTL]`,
+    `Esperanza vida` = `Life expectancy at birth, total (years) [SP.DYN.LE00.IN]`,
+    `Acceso electricidad` = `Access to electricity (% of population) [EG.ELC.ACCS.ZS]`,
+    `Area boscosa` = `Forest area (% of land area) [AG.LND.FRST.ZS]`,
+    `Suscripciones movil` = `Mobile cellular subscriptions (per 100 people) [IT.CEL.SETS.P2]`,
+    `Crecimiento PIB` = `GDP growth (annual %) [NY.GDP.MKTP.KD.ZG]`,
+    `Mortalidad infantil` = `Mortality rate, infant (per 1,000 live births) [SP.DYN.IMRT.IN]`,
+    `Inversion extranjera` = `Foreign direct investment, net inflows (% of GDP) [BX.KLT.DINV.WD.GD.ZS]`,
+    `Gasto salud` = `Current health expenditure (% of GDP) [SH.XPD.CHEX.GD.ZS]`,
+    `Uso internet` = `Individuals using the Internet (% of population) [IT.NET.USER.ZS]`,
+    `Importaciones` = `Imports of goods and services (% of GDP) [NE.IMP.GNFS.ZS]`,
+    `Exportaciones` = `Exports of goods and services (% of GDP) [NE.EXP.GNFS.ZS]`,
+    `Tierra cultivable` = `Arable land (% of land area) [AG.LND.ARBL.ZS]`,
+    `Crecimiento poblacion` = `Population growth (annual %) [SP.POP.GROW]`,
+    Industria = `Industry (including construction), value added (% of GDP) [NV.IND.TOTL.ZS]`,
+    Remesas = `Personal remittances, received (% of GDP) [BX.TRF.PWKR.DT.GD.ZS]`
+  )
+
+# Reemplazar valores ":" por NA
+Base_2022[Base_2022 == ".."] <- NA
+
+# Eliminar filas con NA en cualquiera de las variables numéricas
+Base_2022 <- Base_2022 %>%
+  drop_na()
+
+# Filtrar solo países reales (no regiones o grupos)
+Base_2022 <- Base_2022 %>%
+  filter(!Pais %in% c("World", "High income", "Low income", "European Union",
+                      "Latin America & Caribbean", "Middle income", "OECD members",
+                      "East Asia & Pacific", "Sub-Saharan Africa", "South Asia",
+                      "North America", "Euro area", "Arab World"))
+
+# Ver resultado
+glimpse(Base_2022)
+
+View(Base_2022)
