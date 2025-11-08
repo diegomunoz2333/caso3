463dc42425150faca679d1006a9c50b9fe9da8d6
johanportilla-max <johan.portilla@correounivalle.edu.co>
2025-11-08 18:13:21 -0500

    c

--- MODELOFINAL.R
+++ MODELOFINAL.R
@@ -12,10 +12,58 @@ library(kableExtra)
 library(dplyr)
 library(ggrepel)
 library(ggforce)
-datos_analisis <- Base_2022_filtrada %>%
-  filter(Pais != "Luxembourg") %>% 
-  select(-Codigo) %>% 
+
+Base <- read_csv("f36a5086-3311-4b1a-9f0c-bda5cd4718df_Series - Metadata.csv",
+                 show_col_types = FALSE)
+
+Base_2022 <- Base %>%
+  select(
+    Pais = `Country Name`,
+    Codigo = `Country Code`,
+    PIB_per = `GDP per capita (current US$) [NY.GDP.PCAP.CD]`,
+    Poblacion = `Population, total [SP.POP.TOTL]`,
+    `Esperanza vida` = `Life expectancy at birth, total (years) [SP.DYN.LE00.IN]`,
+    `Acceso electricidad` = `Access to electricity (% of population) [EG.ELC.ACCS.ZS]`,
+    `Area boscosa` = `Forest area (% of land area) [AG.LND.FRST.ZS]`,
+    `Suscripciones movil` = `Mobile cellular subscriptions (per 100 people) [IT.CEL.SETS.P2]`,
+    `Crecimiento PIB` = `GDP growth (annual %) [NY.GDP.MKTP.KD.ZG]`,
+    `Mortalidad infantil` = `Mortality rate, infant (per 1,000 live births) [SP.DYN.IMRT.IN]`,
+    `Inversion extranjera` = `Foreign direct investment, net inflows (% of GDP) [BX.KLT.DINV.WD.GD.ZS]`,
+    `Gasto salud` = `Current health expenditure (% of GDP) [SH.XPD.CHEX.GD.ZS]`,
+    `Uso internet` = `Individuals using the Internet (% of population) [IT.NET.USER.ZS]`,
+    Importaciones = `Imports of goods and services (% of GDP) [NE.IMP.GNFS.ZS]`,
+    Exportaciones = `Exports of goods and services (% of GDP) [NE.EXP.GNFS.ZS]`,
+    `Tierra cultivable` = `Arable land (% of land area) [AG.LND.ARBL.ZS]`,
+    `Crecimiento poblacion` = `Population growth (annual %) [SP.POP.GROW]`,
+    Industria = `Industry (including construction), value added (% of GDP) [NV.IND.TOTL.ZS]`,
+    Remesas = `Personal remittances, received (% of GDP) [BX.TRF.PWKR.DT.GD.ZS]`
+  )
+
+Base_2022[Base_2022 == ".."] <- NA
+
+Base_2022 <- Base_2022 %>%
+  mutate(across(-c(Pais, Codigo), as.numeric)) %>%
+  drop_na() %>%
+  filter(!Pais %in% c("World", "High income", "Low income", "European Union",
+                      "Latin America & Caribbean", "Middle income", "OECD members",
+                      "East Asia & Pacific", "Sub-Saharan Africa", "South Asia",
+                      "North America", "Euro area", "Arab World", "West Bank and Gaza",
+                      "Lower middle income", "Upper middle income", 
+                      "Least developed countries: UN classification",
+                      "Fragile and conflict affected situations", 
+                      "Heavily indebted poor countries (HIPC)",
+                      "IDA total", "Low & middle income", "Middle East & North Africa",
+                      "Pacific island small states", "Small states", 
+                      "Caribbean small states", "Other small states", 
+                      "IDA & IBRD total", "IDA only", "IBRD only",
+                      "Pre-demographic dividend", "Post-demographic dividend", 
+                      "Early-demographic dividend", "Late-demographic dividend")) %>% 
+  filter(Pais != "Luxembourg")
+
+datos_analisis <- Base_2022 %>%
+  select(-Codigo) %>%
   column_to_rownames("Pais")
+
 
 cat("Países:", nrow(datos_analisis), "\n")
 cat("Variables:", ncol(datos_analisis), "\n\n")
